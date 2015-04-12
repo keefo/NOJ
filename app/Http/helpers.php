ccd57122d4b98d8a1e8e4ed5945084a0b06df00f
Xu Lian <lianxu@beyondcow.com>
2015-04-12 12:34:31 -0700

    added startsWith endsWith functions

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -394,3 +394,11 @@ function progressBar($done, $total, $label="", $size=30) {
 
 }
 
+function startsWith($haystack, $needle) {
+    // search backwards starting from haystack length characters from the end
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+function endsWith($haystack, $needle) {
+    // search forward starting from end minus needle length characters
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
